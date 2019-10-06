9d5bdcd89c85acfde2f2c8475e8359c0d26e42fd
Mark Moore <mark.c.moore@hotmail.com>
2019-10-05 23:56:05 -0500

    Create framework for { get; set; } fields and basic CRUD function definitions

---
+++ ClassLibrary1/Customers.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Customers
+    {
+        public int      CustomerID          { get; set; }// added by SQL
+        public string   CustomerFirstName   { get; set; }
+        public string   CustomerLastName    { get; set; }
+        public string   CustomerStreet      { get; set; }
+        public string   CustomerCity        { get; set; }
+        public int      CustomerZipCode     { get; set; }
+
+
+
+        ///<summary>
+        ///</summary>
+        public static void CreateAndAddCustomer()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void ReadCustomer()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void UpdateCustomer()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void DeleteCustomer()
+        {
+
+        }
+        
+    }
+}
---
+++ ClassLibrary1/Locations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    class Locations
+    {
+        public int LocationID { get; set; }
+        public string LocationName { get; set; }
+        public string LocationStreet { get; set; }
+        public string LocationCity { get; set; }
+        public int LocationZip { get; set; }
+
+        ///<summary>
+        ///</summary>
+        public static void CreateAndAddLocation()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void ReadLocation()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void UpdateLocation()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void DeleteLocation()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void ChangeLocationDetails()
+        {
+
+        }
+
+        ///<summary>
+        ///</summary>
+        public static void SearchLocations()
+        {
+
+        }
+    }
+}
