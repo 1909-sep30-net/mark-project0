0d3b1095a93f566f978746bb4d7247735e7ad44a
Mark Moore <mark.c.moore@hotmail.com>
2019-10-05 23:55:07 -0500

    Create framework for { get; set; } fields and basic CRUD function definitions

---
+++ mark-project0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace mark_project0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         /*     functionality
+                place orders to store locations for customers
+                add a new customer
+                search customers by name
+                display details of an order
+                display all order history of a store location
+                display all order history of a customer
+                input validation
+                exception handling
+                persistent data(SQL); no products, prices, customers, etc. hardcoded in C#
+                logging
+                (optional: order history can be sorted by earliest, latest, cheapest, most expensive)
+                (optional: get a suggested order for a customer based on his order history)
+                                (optional: save some or all data to disk in JSON format)
+                (optional: load some or all data from disk)
+                (optional: display some statistics based on order history)
+         */
+        }
+    }
+}
